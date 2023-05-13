00e1c350b1ff8b14511ad1ac03ba9f2bd951d49a
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2023-05-13 23:17:19 +0900

    asdadasdas

--- src/components/fragments/WebHeader.vue
+++ src/components/fragments/WebHeader.vue
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
     getCategoryData() {
-      axios.get("http://igdb.kro.kr/lostark/api/getMyCharacter")
+      axios.get("http://172.30.1.51:80/lostark/api/getMyCharacter")
       .then((res) => {
         this.message = res.data;
         console.log(res.data);

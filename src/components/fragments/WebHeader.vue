d057a0a02f1971975827d903c653f446f9baac19
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2023-05-13 20:38:02 +0900

    왜 안돼?

--- src/components/fragments/WebHeader.vue
+++ src/components/fragments/WebHeader.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
+    {{ message }}
     <button v-on:click="getCategoryData">Button</button>
   </div>
 </template>
 
 <script>
 import axios from "axios"
+
 export default {
+  data() {
+    return {
+      message: ""
+    }
+  },
   methods: {
     getCategoryData() {
       axios.get("http://igdb.kro.kr/lostark/api/getMyCharacter")
       .then((res) => {
+        this.message = res.data;
         console.log(res.data);
       })
       .catch((err) => {
